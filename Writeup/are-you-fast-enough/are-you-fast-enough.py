e57e4bb13067961c11522c98372e94d704142c99
Hadi Saleh <63310975+Hadi-Saleh@users.noreply.github.com>
2021-01-25 15:21:08 +0200

    Update are-you-fast-enough.py

--- Writeup/are-you-fast-enough/are-you-fast-enough.py
+++ Writeup/are-you-fast-enough/are-you-fast-enough.py
@@ -4,10 +4,8 @@ url = 'http://timesink.be/speedy/'
 req = requests.get(url)
 tree = html.fromstring(req.content)
 pas = tree.xpath('//div[@id="rndstring"]/text()')
-print(pas)
 password = [str(s) for s in pas]
 password = ''.join(password)
-print(password)
 cookies = req.cookies
 data = {"inputfield":password}
 req = requests.post(url=url,data=data,cookies = cookies)
